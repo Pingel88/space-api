d4f27275de025608003b8ebc3c39ba3db962bad6
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-10 15:18:44 -0800

    End of pairing day commit
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,3 +4,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import EpicEarth from './js/epic-earth.js';
+
+function randomImage(length) {
+  return Math.floor(Math.random() * length);
+}
+
+
+$(document).ready(function() {
+  $('#dateForm').submit(function(event) {
+    event.preventDefault();
+    const userDateInput = $('#date-input').val();
+    const dateSlash = userDateInput.replace(/-/g, "/");
+    (async function() {
+      const response = await EpicEarth.makeEpicApiCall(userDateInput);
+      console.log(response);
+      if (response.length === 0) {
+        $('#image').html("<h1>please select another date</h1>");
+      } else {
+        const imageNumber = randomImage(response.length);
+        const imageFile = response[imageNumber].image;
+        $('#image').html(`<img src="https://epic.gsfc.nasa.gov/archive/enhanced/${dateSlash}/png/${imageFile}.png" alt="EPIC Image">`);
+      }
+    })();
+  })
+})
